8c96284caa14919f9ac067011fb35bc73cf9603e
Ehsan Meamari <47637290+ehsanmeamari@users.noreply.github.com>
2026-03-21 19:42:45 -0400

    Update ecc_tool.py

--- modules/ecc_tool.py
+++ modules/ecc_tool.py
@@ -29,8 +29,8 @@ def run_ecc_visualizer():
         if discriminant != 0:
             y, x = np.ogrid[-5:5:100j, -5:5:100j]
             
-            # REDUCED SIZE: Changed to (3.5, 2.5) for a compact look
-            fig, ax = plt.subplots(figsize=(3.5, 2.5))
+            # SIZE: Changed to (3.5, 2.5) for a compact look
+            fig, ax = plt.subplots(figsize=(1.5, 1.5))
             
             # Curve plotting
             ax.contour(x.ravel(), y.ravel(), y**2 - x**3 - a*x - b, [0], colors='royalblue')
