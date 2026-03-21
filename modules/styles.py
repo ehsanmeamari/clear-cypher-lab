d3cd0d233eccf3780fd71637d898fff59d43ab5c
Ehsan Meamari <47637290+ehsanmeamari@users.noreply.github.com>
2026-03-21 19:16:39 -0400

    Update styles.py

--- modules/styles.py
+++ modules/styles.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+def apply_styles():
+    st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 2rem; 
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
