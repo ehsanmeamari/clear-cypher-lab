1d3335a57a706ea16b2c3c128dea37b5a91e567a
Ehsan Meamari <47637290+ehsanmeamari@users.noreply.github.com>
2026-03-22 18:22:22 -0400

    Update styles.py

--- modules/styles.py
+++ modules/styles.py
@@ -1,11 +1,11 @@
-# --- فایل modules/styles.py ---
+# --- modules/styles.py ---
 import streamlit as st
 
 def apply_styles():
     st.markdown("""
         <style>
                .block-container {
-                    padding-top: 0rem !important; /* حتما روی صفر تنظیم شود */
+                    padding-top: 0rem !important; /* لغو هرگونه فاصله از بالا */
                     padding-bottom: 0rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
