f4132f50416a672bc6af3000261beec50148386c
Ehsan Meamari <47637290+ehsanmeamari@users.noreply.github.com>
2026-05-05 21:12:02 -0600

    Update ecc_overR.py

--- modules/ecc_overR.py
+++ modules/ecc_overR.py
@@ -91,7 +91,7 @@ def run_ecc_overR():
 
             mode = st.session_state[f"mode_{suffix}"]
 
-            r1c1, r1c2, r1c3, r1c4, r1c5, r1c6, r1c7, r1c8 = st.columns([1, 0.4, 0.3, 0.01, 0.8, 0.3, 0.3, 0.3])
+            r1c1, r1c2, r1c3, r1c4, r1c5, r1c6, r1c7, r1c8 = st.columns([1, 0.4, 0.3, 0.01, 0.8, 0.4, 0.3, 0.3])
             with r1c1: st.markdown("<div class='small-label' style='padding-top:8px; white-space:nowrap;'>Input mode:</div>", unsafe_allow_html=True)
             with r1c2:
                 if st.button("X", key=f"btn_x_{suffix}", use_container_width=True):
