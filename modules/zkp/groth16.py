b2ae76f9b4a43d09bf3158ebe6a452d569598b40
Ehsan Meamari <47637290+ehsanmeamari@users.noreply.github.com>
2026-05-26 00:48:39 -0600

    Update groth16.py

--- modules/zkp/groth16.py
+++ modules/zkp/groth16.py
@@ -46,15 +46,21 @@ def run_groth16():
     """, unsafe_allow_html=True)
 
     # ── Step 0 ───────────────────────────────────────────────────────────────
-    with st.expander("Colab", expanded=True):
-        st.link_button(
-            "🟠 zkAuction",
-            "https://colab.research.google.com/github/ehsanmeamari/clear-cypher-lab/blob/main/modules/zkp/zkAuction.ipynb"
-        )
-        st.link_button(
-            "🟠 zkCredit",
-            "https://colab.research.google.com/github/ehsanmeamari/clear-cypher-lab/blob/main/modules/zkp/zkCredit.ipynb"
-        )
+        with st.expander("Colab", expanded=True):
+                st.markdown(
+                    """
+        <a href="https://colab.research.google.com/github/ehsanmeamari/clear-cypher-lab/blob/main/modules/zkp/zkAuction.ipynb" target="_blank">
+        <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+        </a>
+        <span style="margin-left:8px; font-weight:bold;">zkAuction</span>
+        <br><br>
+        <a href="https://colab.research.google.com/github/ehsanmeamari/clear-cypher-lab/blob/main/modules/zkp/zkCredit.ipynb" target="_blank">
+        <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+        </a>
+        <span style="margin-left:8px; font-weight:bold;">zkCredit</span>
+        """,
+                    unsafe_allow_html=True,
+                )
 
     # ── Step 1 ───────────────────────────────────────────────────────────────
     # with st.expander("Step 1: Circuit Inputs & Witness Vector", expanded=False):
